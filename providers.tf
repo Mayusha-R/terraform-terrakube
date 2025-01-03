8c3e49d14d478b6c5e283f6afeff29eebc0c90eb
Myusha-R <rathodmayusha@gmail.com>
2025-01-03 10:38:38 +0530

    provider block changed

--- providers.tf
+++ providers.tf
@@ -9,7 +9,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  # subscription_id = "664b6097-19f2-42a3-be95-a4a6b4069f6b"
-  skip_provider_registration = true
+  resource_provider_registrations = "none"
 }
