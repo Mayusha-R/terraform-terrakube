3d486c80a9d47c9ec8429dccc3d74d4c9fbae318
Myusha-R <rathodmayusha@gmail.com>
2025-01-03 18:05:20 +0530

    backend added

--- providers.tf
+++ providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=4.2.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "sa1_test_eic_MayushaRathod"  
+    storage_account_name = "terrakubestate"                      
+    container_name       = "tfstate"                       
+    key                  = "terraform.tfstate"        
+  }
+
 }
 
 provider "azurerm" {
